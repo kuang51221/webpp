30b7d68f2ff02af4be2bbb4407e9c47d86cd711b
kuang51221 <kuang51221@gmail.com>
2025-06-13 15:14:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // 配置 Firebase (Trystero 的后端之一)
 const config = {
   appId: 'https://netdot-2a73d-default-rtdb.firebaseio.com/', // 你可以更改这个ID
-  rootPath: 'trystero-demo'
+  rootPath: 'webpp'
 };
 
 // 选择 Firebase 作为连接方式
